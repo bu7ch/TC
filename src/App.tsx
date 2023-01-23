5716e743614ac19487dd9064da39cb00ca3e06be
Rodolphe <dutchr@live.fr>
2023-01-23 16:19:10 +0100

    :sparkles: adding some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { AppContainer } from './styles';
+import { Column } from './components/Column';
+import { Card } from './components/Card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <AppContainer>
+    <Column text='To> Do'>
+      <Card text="Learn Typescript"/>
+    </Column>
+    <Column text='In progress'>
+      <Card text="Learn JS"/>
+    </Column>
+    <Column text='Done'>
+      <Card text="begin to use Static typing"/>
+    </Column>
+  </AppContainer>
   );
 }
 
