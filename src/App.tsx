0332d2e9ebf168de1ea5f5afd5d873aa986ca84c
Rodolphe <dutchr@live.fr>
2023-01-24 10:46:34 +0100

    :sparkles: updates

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import './App.css';
 import { AppContainer } from './styles';
 import { Column } from './components/Column';
 import { Card } from './components/Card';
+import { AddNewItem } from './components/AddNewItem';
 
 function App() {
   return (
   <AppContainer>
-    <Column text='To> Do'>
+    <Column text='To Do'>
       <Card text="Learn Typescript"/>
     </Column>
     <Column text='In progress'>
@@ -15,6 +16,8 @@ function App() {
     </Column>
     <Column text='Done'>
       <Card text="begin to use Static typing"/>
+      <Card text="begin to use Static typing"/>
+    {/* <AddNewItem toggleButtonText='+ Add another list' onAdd={console.log}/> */}
     </Column>
   </AppContainer>
   );
--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ColumnContainer, ColumnTitle } from "../styles";
+import { AddNewItem } from "./AddNewItem";
 
 interface ColumnProps {
   text?: string;
@@ -13,6 +14,8 @@ export const Column = ({
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
       {children}
+      <AddNewItem toggleButtonText='+ Add another list' onAdd={console.log}/>
+
     </ColumnContainer>
   );
 };
